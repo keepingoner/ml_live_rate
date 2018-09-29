cc72e42f78247ca4eb7b31789b618c91058f0e12
keepingoner <keepingoner@163.com>
2018-09-29 10:21:26 +0800

    biji

---
+++ 线性回归模型/多项式扩展.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# @Time    : 2018/9/29 10:07
+# @Author  : jin
+# @File    : 多项式扩展.py
---
+++ 线性回归模型/过拟合.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# @Time    : 2018/9/29 10:08
+# @Author  : jin
+# @File    : 过拟合.py
